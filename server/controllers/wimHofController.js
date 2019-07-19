21aa955a9e9c6f4964e22ba97185fb784e97c1cd
Victor Juan <vj.jimenez96@gmail.com>
2019-07-19 17:49:43 -0500

    aea

--- server/controllers/wimHofController.js
+++ server/controllers/wimHofController.js
@@ -1,4 +1,26 @@
 const WimHof = require('../models/wimHof');
+
+//Display selected wimhof
+exports.wimHof_select = (req, res) => {
+    //Display list of selected wimhofs
+    let query = req.query;
+    let id = query.id;
+    WimHof.find({
+        _id: id
+    }).exec((err, payload) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            })
+        }
+        res.json({
+            ok: true,
+            payload
+        })
+    });
+}
+
 //Display list of done wimHofs 
 exports.wimHof_list = (req, res) => {
         //Display list of done wimHofs 
@@ -40,18 +62,28 @@ exports.wimHof_create = (req, res) => {
 exports.wimHof_update = (req, res) => {
     let id = req.params.id;
     let body = req.body;
-    WimHof.findByIdAndUpdate(id, body, {
+    let title = req.body.wimhof.title;
+    let notes = req.body.wimhof.notes;
+    let laps = req.body.wimhof.laps;
+    let state = req.body.wimhof.state;
+    WimHof.findByIdAndUpdate(id, {
+        title,
+        notes,
+        laps,
+        state
+    }, {
         new: true
-    }, (err, WimHofDB) => {
+    }, (err, payload) => {
         if (err) {
             return res.status(400).json({
                 ok: false,
                 err
             });
         }
+        console.log("devuelvo payload: ", payload);
         res.json({
             ok: true,
-            WimHof: WimHofDB
+            payload: payload
         });
     });
 }
--- server/routes/api/wimHof.js
+++ server/routes/api/wimHof.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 //Controllers
-const goal_controller = require('../../controllers/wimHofController')
+const wimhof_controller = require('../../controllers/wimHofController')
 
 //wimHof API endPpoints
-router.get('/wimhof', goal_controller.wimHof_list);
-router.post('/wimhof', goal_controller.wimHof_create);
-// router.put('/wimhof/:id', goal_controller.goal_update);
-// router.delete('/wimhof/:id', goal_controller.goal_delete);
+router.get('/wimhof/search', wimhof_controller.wimHof_select);
+router.get('/wimhof', wimhof_controller.wimHof_list);
+router.post('/wimhof', wimhof_controller.wimHof_create);
+router.put('/wimhof/:id', wimhof_controller.wimHof_update);
+// router.delete('/wimhof/:id', wimhof_controller.wimhof_delete);
 
 module.exports = router;
